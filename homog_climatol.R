33302efab1a68f158854801cabed946a217efc66
guidofioravanti <guidofioravanti@yahoo.it>
2017-10-13 15:06:38 +0200

    Aggiunto codice per creare file breakpoints filtrato, senza breakpoints a estremi serie

--- homog_climatol.R
+++ homog_climatol.R
@@ -1,5 +1,6 @@
 rm(list=objects())
 library("climatol")
+library("stringr")
 library("purrr")
 library("dplyr")
 library("tidyr")
@@ -17,10 +18,23 @@ ANNOF<-2015
 correggi<-TRUE
 creaMensili<-FALSE
 
+paste0(param,"_",ANNOI,"-",ANNOF)->stringaBase
 
 if(!correggi){
   if(creaMensili) dd2m(varcli =param ,anyi = ANNOI,anyf = ANNOF,valm = 2,namax = 10,na.strings = NA,homog = FALSE,ini=paste0(ANNOI,"-01-01"))
-  homogen(varcli = paste0(param,"-m"),anyi =ANNOI,anyf = ANNOF,wd=c(200,200,100),snht1 = 25,snht2 = 25)
+  homogen(varcli = paste0(param,"-m"),anyi =ANNOI,anyf = ANNOF,wd=c(200,200,100),snht1 = 25,snht2 = 25,swa=120)
+  
+  #vogliamo creare un file dei breakpoint analogo a quello creato dal programma in cui eliminiamo i breakpoint agli estremi della serie
+  #Infatti tenere i breakpoint troppo vicini all'inizio o alla fine del periodo in esame genera in alcuni casi correzioni eccessive
+  #(non realistiche) delle serie
+  paste0(str_replace(stringaBase,paste0("^",param), paste0(param,"-m")),"_brk.csv")->nomeBrk
+  readr::read_delim(nomeBrk,delim=",",col_names=TRUE)->breakPoints
+  
+  breakPoints %>% dplyr::filter(Date >= as.Date(paste0(ANNOI+5,"-01-01")) & Date<= as.Date(paste0(ANNOF-5,"-12-31")) )->newBrk
+  #nuovo file con breakpoints filtrati. Toccherà all'utente nei passi successivi decidere quale file di breakpoints utilizzare
+  if(nrow(newBrk)) readr::write_delim(newBrk,path =str_replace(nomeBrk,"\\.csv","_filtrati.csv"),delim=",",col_names=TRUE)
+  
+
 }else{
   #correction of daily series
   homogen(varcli = param,anyi =ANNOI,anyf = ANNOF,metad = TRUE)
@@ -35,8 +49,8 @@ if(!correggi){
   assemblaHomog(param,ANNOI,ANNOF,mask=TRUE)->dfHomo
   #dfHomo è un data.frame che contiene le serie omogeneizzate (solo le serie che arrivano alla fine del periodo di analisi)
   
-  fileSerieOriginali<-paste0(param,"_",ANNOI,"-",ANNOF,"_raw.RDS")
-  nome_pdf<-paste0(param,"_",ANNOI,"-",ANNOF,"_serieAnnuali_homo.pdf")
+  fileSerieOriginali<-paste0(stringaBase,"_raw.RDS")
+  nome_pdf<-paste0(stringaBase,"_serieAnnuali_homo.pdf")
   
   #grafico delle serie omogeneizzate. Se esiste il file RDS con le serie originali "raw" allora facciamo il grafico della serie
   #originale (in blu) e della corrispondente serie omogeneizzata (in nero)
